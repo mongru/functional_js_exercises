e2acd3f69f67863845091ff510de63641a974362
mongru <monika.grubizna@gmail.com>
2019-08-18 20:30:13 +0100

    ramda exercise 1

---
+++ ramda/ex-1.js
@@ -0,0 +1,17 @@
+const R = require("ramda");
+const idEquals = R.propEq("id");
+const isAlex = idEquals(2, { id: 2, name: "Alex" });
+const isFirstPost = idEquals(1, { id: 1, title: "My first post" });
+
+const wasBornInCountry = R.propEq("birthCountry", "UK");
+const wasNaturalized = person => Boolean(person.naturalizationDate);
+const isOver18 = person => person.age >= 18;
+const isCitizen = R.either(wasBornInCountry, wasNaturalized);
+const isEligibleToVote = R.both(isOver18, isCitizen);
+
+const testUser = {
+  age: 20,
+  birthCountry: "UK"
+};
+
+console.log(isEligibleToVote(testUser));
